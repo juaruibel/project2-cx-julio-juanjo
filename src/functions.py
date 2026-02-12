6e9e6b3c10e5e7f45d6462ba37cfbc5269990db9
Juan José Ruiz Bellido <juaruibel@gmail.com>
2026-02-12 15:03:59 +0100

    Notebook de métricas resuelto; creo notebook 2 para testing y añado funciones a functions.py

--- src/functions.py
+++ src/functions.py
@@ -43,3 +43,78 @@ def load_data_merge(file_merge):
     """
     path = DATA_CLEAN_DIR / file_merge
     return pd.read_csv(path)
+
+def tasa_finalizacion(df):
+    """
+    Toma un dataframe y devuelve tasa de finalización.
+    """
+    # Usuarios únicos que llegan a confirm 
+    usuarios_confirm = df[df["process_step"] == "confirm"]
+    usuarios_confirm = (df[df["process_step"] == "confirm"][["client_id"]].drop_duplicates())
+    num_usuarios_confirm = usuarios_confirm.shape[0]
+    # Client id únicos
+    num_client_id_unique = df["client_id"].nunique()
+    output = round((num_usuarios_confirm/num_client_id_unique)*100, 2)
+    return output
+
+def tiempo_dedicado(df):
+    """
+    Toma un dataframe y devuelve el tiempo dedicado de los pasos.
+    """
+    df["date_time"] = pd.to_datetime(df["date_time"])
+    df = df.sort_values(["visitor_id", "visit_id", "date_time"])
+
+    # Fechas y diferencia
+    
+    df["next_time"] = df.groupby(["client_id","visit_id"])["date_time"].shift(-1)
+    df["next_step"] = df.groupby(["client_id","visit_id"])["process_step"].shift(-1)
+    df["duration"] = df["next_time"] - df["date_time"]
+
+    # Filtros por step
+
+    start_s1 = df[(df["process_step"] == "start") & (df["next_step"] == "step_1")]
+    s1_s2 = df[(df["process_step"] == "step_1") & (df["next_step"] == "step_2")]
+    s2_s3 = df[(df["process_step"] == "step_2") & (df["next_step"] == "step_3")]
+    s3_confirm = df[(df["process_step"] == "step_3") & (df["next_step"] == "confirm")]
+    
+    # Medias
+    
+    media_paso1 = start_s1["duration"].mean()
+    media_paso2 = s1_s2["duration"].mean()
+    media_paso3 = s2_s3["duration"].mean()
+    media_paso4 = s3_confirm["duration"].mean()
+    print(f"El tiempo dedicado de start a paso 1 es: {media_paso1}")
+    print(f"El tiempo dedicado del paso 1 al paso 2 es: {media_paso2}")
+    print(f"El tiempo dedicado del paso 2 al paso 3 es: {media_paso3}")
+    print(f"El tiempo dedicado del paso 3 a confirm es: {media_paso1}")
+    return media_paso1, media_paso2, media_paso3, media_paso4
+
+def tasa_error(df):
+    """
+    Toma un df y devuelve la tasa de error:
+    Consideramos como tasa de error lo que en la metodología del proyecto es señalado como tal
+    sin embargo cabe la posibilidad de considerar como error que next_step sea el mismo paso
+    pero sí se contabiliza como transición.
+    """
+    
+    df["date_time"] = pd.to_datetime(df["date_time"])
+    df = df.sort_values(["visitor_id", "visit_id", "date_time"])
+
+    # Fechas y diferencia
+    
+    df["next_time"] = df.groupby(["client_id","visit_id"])["date_time"].shift(-1)
+    df["next_step"] = df.groupby(["client_id","visit_id"])["process_step"].shift(-1)
+    df["duration"] = df["next_time"] - df["date_time"]
+
+    # Error
+
+    error1 = df[(df["process_step"] == "step_1") & (df["next_step"] == "start")].shape[0]
+    error2 = df[(df["process_step"] == "step_2") & (df["next_step"] == "start")].shape[0]
+    error3 = df[(df["process_step"] == "step_2") & (df["next_step"] == "step_1")].shape[0]
+    error4 = df[(df["process_step"] == "step_3") & (df["next_step"] == "start")].shape[0]
+    error5 = df[(df["process_step"] == "step_3") & (df["next_step"] == "step_1")].shape[0]
+    error6 = df[(df["process_step"] == "step_3") & (df["next_step"] == "step_2")].shape[0]
+    error_total = error1 + error2 + error3 + error4 + error5 + error6
+    total_transiciones = df["next_step"].notna().sum()
+    tasa_error = round((error_total / total_transiciones) * 100, 2)
+    return tasa_error
